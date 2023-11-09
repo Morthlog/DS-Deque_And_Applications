4fd532d30644b78e2094f18c2c684b8fb02bf3f9
BabisDros <150071850+BabisDros@users.noreply.github.com>
2023-11-09 17:25:48 +0200

    Create DNAPalindrome.java

---
+++ DNAPalindrome.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class DNAPalindrome 
+{
+
+	public static void main(String[] args) 
+	{
+		StringDoubleEndedQueue<String> sequenceQueue= new StringDoubleEndedQueueImpl();
+		Scanner keyboard = new Scanner(System.in);
+		
+		int sequence = keyboard.nextInt();
+	}
+}
