9d3f4fe2c235af8ff772616812eef4c7c5c5d20f
Morthlog <iosifpetroulakis@hotmail.com>
2023-11-21 20:09:30 +0200

    removed main from Impl

--- StringDoubleEndedQueueImpl.java
+++ StringDoubleEndedQueueImpl.java
@@ -190,32 +190,5 @@ public class  StringDoubleEndedQueueImpl<T> implements StringDoubleEndedQueue<T>
 	{
         return size;
     }
-	
-	public static void main(String[] args) {
-		StringDoubleEndedQueueImpl<String> queue = new StringDoubleEndedQueueImpl<>();
-		System.out.print("Print empty Queue: ");
-		
-		queue.printQueue(System.out);
-		
-	    // Test the methods
-	    queue.addFirst("A");
-	    queue.addLast("B");
-	    queue.addFirst("C");
-	    queue.addLast("D");
-	
-	    System.out.print("Queue after insertion: ");
-	    queue.printQueue(System.out);
-	    System.out.println("Size: " + queue.size());
-	
-	    System.out.println("First element: " + queue.getFirst());
-	    System.out.println("Last element: " + queue.getLast());
-	
-	    System.out.println("Removed first element: " + queue.removeFirst());
-	    System.out.println("Removed last element: " + queue.removeLast());
-	
-	    System.out.print("Queue after removals: ");
-	    queue.printQueue(System.out);
-	    System.out.println("Size: " + queue.size());
-	}
 }
 
